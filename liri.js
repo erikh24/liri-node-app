cbe3f013ddd5a8ff457ec68a5e49e49bd0e5b9c1
Erik H <erikjh_24@yahoo.com>
2019-07-28 16:40:39 -0700

    updated liri.js so that you can search for an artist name and it will return the following: Name of venues that the artist is playing at, along with the city of the venue and the date. Does not yet have the ability added to include concert-this when searching for the artist.

--- liri.js
+++ liri.js
@@ -8,16 +8,22 @@ var moment = require('moment');
 var axios = require("axios");
 
 
+
 //1. node liri.js concert-this <artist/band name here>
+// concert-this
+var artist = process.argv.slice(2).join('');
 
-
-
-// need to create variable for artist?
 axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
 .then(function(response)    {
-    console.log(response.data.// name of venue);
-    console.log(response.data.//venue location);
-    console.log(response.data.//event date formatted as MM/DD/YYYY);
+    for (var i = 0; i < response.data.length; i++) {
+        var data = response.data[i];
+        var venue = data.venue;
+   
+    console.log(venue.name); // name of venue
+    console.log(venue.city); //venue location
+    console.log(moment(data.datetime).format("MM/DD/YY")); //event date formatted as MM/DD/YYYY
+    console.log(artist); //logging to check name of artist that is being searched for
+    }
 });
 
 
@@ -25,14 +31,15 @@ axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=cod
 
 
 //2. node liri.js spotify-this-song '<song name here>'
+// spotify-this-song
 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
+// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+//     if (err) {
+//       return console.log('Error occurred: ' + err);
+//     }
    
-  console.log(data); 
-  });
+//   console.log(data); 
+//   });
 
 
 
@@ -41,20 +48,21 @@ spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, d
 
 
 //3. node liri.js movie-this '<movie name here>'
+// movie-this
 
 
 // need to create variable for movieName?
-axios.get("http://omdbapi.com/?t=" + movieName + "&plot=short&apikey=trilogy")
-.then(function(response) {
-    console.log(response.data.Title);
-    console.log(response.data.Year);
-    console.log(response.data.imdbRating);
-    console.log(response.data.Ratings[1]); // rotten tomatoes rationg
-    console.log(response.data.Country);
-    console.log(response.data.Language);
-    console.log(response.data.Plot);
-    console.log(response.data.Actors);
-});
+// axios.get("http://omdbapi.com/?t=" + movieName + "&plot=short&apikey=trilogy")
+// .then(function(response) {
+//     console.log(response.data.Title);
+//     console.log(response.data.Year);
+//     console.log(response.data.imdbRating);
+//     console.log(response.data.Ratings[1]); // rotten tomatoes rationg
+//     console.log(response.data.Country);
+//     console.log(response.data.Language);
+//     console.log(response.data.Plot);
+//     console.log(response.data.Actors);
+// });
 
 
 
@@ -62,6 +70,7 @@ axios.get("http://omdbapi.com/?t=" + movieName + "&plot=short&apikey=trilogy")
 
 
 //4. node liri.js do-what-it-says
+// do-what-it-says
 
 
 
