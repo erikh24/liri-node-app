f038082d0d363761c0cfb4b091f960acdaa9faa0
Erik H <erikjh_24@yahoo.com>
2019-08-03 21:11:29 -0700

    App is working as intended now. Fixed code for do-what-it-says

--- README.md
+++ README.md
@@ -6,12 +6,25 @@ In order to meet the Employer Competitive standards and be ready to show your ap
 
 
 Clearly state the problem the app is trying to solve (i.e. what is it doing and why)
+- This app is taking in user input to display information on either movies, songs or concerts.
+
 Give a high-level overview  of how the app is organized
+- 
+
 Give start-to-finish instructions on how to run the app
+- 
+
 Include screenshots, gifs or videos of the app functioning
+- 
+
 Contain a link to a deployed version of the app
+- 
+
 Clearly list the technologies used in the app
+- 
+
 State your role in the app development
+- I was the sole creator of this project.
 
 
 Because screenshots (and well-written READMEs) are extremely important in the context of GitHub, this will be part of the grading in this assignment.
--- liri.js
+++ liri.js
@@ -7,14 +7,10 @@ var spotify = new Spotify(keys.spotify);
 var moment = require('moment');
 var axios = require("axios");
 
-
-
 //1. node liri.js concert-this <artist/band name here>
 // concert-this
 
 function concertThis(artist) {
-
-    // var artist = process.argv.slice(2).join('');
 
     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
         .then(function (response) {
@@ -22,150 +18,123 @@ function concertThis(artist) {
                 var data = response.data[i];
                 var venue = data.venue;
 
-                // console.log(venue.name.split(", ")); // name of venue
-                // console.log(venue.city); //venue location
-                // console.log(moment(data.datetime).format("MM/DD/YY")); //event date formatted as MM/DD/YYYY
+                var concertData = [
+                    "Concert: " + venue.name,
+                    "City: " + venue.city,
+                    "State: " + data.venue.region,
+                    "Date of the show: " + moment(data.datetime).format("MM/DD/YY"),
+                ].join("\n");
 
-                var concertArray = [
-                    venue.name,
-                    venue.city,
-                    moment(data.datetime).format("MM/DD/YY")
-                ]
-                console.log(concertArray);
-                // console.log(artist); //logging to check name of artist that is being searched for
+                fs.appendFile("log.txt", "---------- CONCERT ----------" + "\n" + concertData + "\n\n", function (err) {
+                    if (err) throw err;
+                    console.log(concertData);
+                });
+
             }
         });
-    // writeToRandomTxt(data);
 }
-// concertThis('ariana grande')
 
 //2. node liri.js spotify-this-song '<song name here>'
-// // spotify-this-song
+// spotify-this-song
+
 function spotifyThisSong(songName) {
     if (!songName) songName = "The Sign Ace of Base";
 
-    // var songName = process.argv.slice(2).join(' ');
     spotify.search({ type: 'track', query: songName }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
-        }
-            var artistNames = new Set();
-            var item = data.tracks.items[0];
-            var artists = item.artists;
-            var songName = item.name;
-            var url = item.external_urls.spotify;
-            var albumName = item.album.name;
-            for (var j = 0; j < artists.length; ++j) {
-                artistNames.add(artists[j].name);
+        } else {
+
+            var tracks = data.tracks.items[0];
+            var artistArr = tracks.artists;
+            var artists = [];
+            for (var j = 0; j < artistArr.length; j++) {
+                artists.push(artistArr[j].name);
             }
-            // console.log(data.tracks);
-            console.log('Artists: ' + Array.from(artistNames).join(', '));
-            console.log('Song Name: ' + songName);
-            console.log('Preview Link: ' + url);
-            console.log('Album Name: ' + albumName);
-        });
-    }
-//         var item = data.tracks.items[0];
-//         var artists = item.artists[0];
-//         var songName = item.name;
-//         var album = item.album;
-//         var url = item.external_urls.spotify;
 
-//         // console.log(data.tracks);
-//         console.log("Artist(s) :" + artists);
-//         console.log("Song Name: " + songName);
-//         console.log("Album: " + album);
-//         console.log("Preview Link: " + url);
-//     });
-// }
+            var songData = [
+                'Artist(s): ' + artists.join(", "),
+                'Song Name: ' + tracks.name,
+                'Album: ' + tracks.album.name,
+                "Preview: " + tracks.preview_url
+            ].join("\n");
 
-// console.log(songName);
-// console.log(data.tracks);
-// for (var i = 0; i < data.tracks.items.length; i++) {
-//     console.log(data.tracks.items[i].name)
-
-// }
-// console.log(data.items.artist[0]);
-// console.log(data.tracks.items.name);
-//     });
-//     // writeToRandomTxt(data);
-// }
-
-
+            fs.appendFile("log.txt", "---------- SONG ----------" + "\n" + songData + "\n\n", function (err) {
+                if (err) throw err;
+                console.log(songData);
+            });
+        }
+    });
+}
 
 //3. node liri.js movie-this '<movie name here>'
 // movie-this
+
 function movieThis(movieName) {
+    if (!movieName) movieName = "Mr. Nobody";
+
     // var  = process.argv.slice(2).join('+');
 
     axios.get("https://omdbapi.com/?t=" + movieName + "&apikey=trilogy")
         .then(function (response) {
+
             var data = response.data;
-            var title = data.Title;
-            var year = data.Year;
-            var imdbRating = data.imdbRating;
-            var rotten = data.Ratings;
-            var country = data.Country;
-            var language = data.Language;
-            var plot = data.Plot;
-            var actors = data.Actors;
+            var movieData = [
+                "Title: " + data.Title,
+                "Year: " + data.Year,
+                "IMDB Rating: " + data.imdbRating,
+                "Rotten Tomatoes Rating: " + data.Ratings[1].Value,
+                "Country: " + data.Country,
+                "Language: " + data.Language,
+                "Plot: " + data.Plot,
+                "Actors: " + data.Actors
+            ].join("\n");
 
-            console.log("Title: " + title);
-            console.log("Year: " + year);
-            console.log("imdb Rating: " + imdbRating);
-            console.log("Rotten Tomatoes Rating: " + rotten);
-            console.log("Country: " + country);
-            console.log("Language: " + language);
-            console.log("Plot: " + plot);
-            console.log("Actors: " + actors);
-
+            fs.appendFile("log.txt", "---------- MOVIE ----------" + "\n" + movieData + "\n\n", function (err) {
+                if (err) throw err;
+                console.log(movieData);
+            });
         });
-    // writeToRandomTxt(data);
-}
+};
 
 //4. node liri.js do-what-it-says
 // do-what-it-says
 
-function writeToRandomTxt(userSentence) {
-    fs.appendFile("random.txt", JSON.stringify(userSentence) + "\n", function (err) {
-        if (err) {
-            return console.log(err);
-        }
-    })
-}
-
 function doWhatItSays() {
     fs.readFile("random.txt", "utf8", function (err, data) {
-        if (data.split(",").length === 2) {
-            pick(data.split(",")[0], data.split(",")[1])
+        if (err) {
+            return console.log("error");
         } else {
-            pick(data.split(",")[0])
+            var content = data.split(',');
+            userCommand = content[0];
+            input = content[1];
+            pick(userCommand, input);
+
         }
     })
-}
+};
 
 var pick = function (userCommand, userPick) {
     switch (userCommand) {
         case 'concert-this':
             concertThis(userPick);
             break;
-        case 'movie-this':
-            movieThis(userPick);
-            break;
         case 'spotify-this-song':
             spotifyThisSong(userPick);
             break;
-        case 'do-what-it-says':
-            doWhatItSays();
+        case 'movie-this':
+            movieThis(userPick);
             break;
+        case 'do-what-it-says':
+            doWhatItSays(userPick);
+            break;
+        default:
+            console.log("Oops, try again.\nValid inputs are concert-this, spotify-this-song, movie-this, or do-what-it-says.")
     }
-}
+};
 
 var runProgram = function (arg1, arg2) {
     pick(arg1, arg2);
 };
 
 runProgram(process.argv[2], process.argv.slice(3).join(" "));
-
-
-
